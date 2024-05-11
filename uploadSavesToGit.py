c469cd29cca79c46359e374153fb1f97ff2baa23
zy <kinomaxxing@gmail.com>
2024-05-11 05:11:17 +0100

    update repo

--- uploadSavesToGit.py
+++ uploadSavesToGit.py
@@ -42,6 +42,7 @@ def upload(overrideAltered, overrideChangedMessage, output=True):
     print(f"{bcolors.LINE}==========================================================================={bcolors.ENDC}")
 
     for i in range(0,3):
+        print(i)
         subprocess.call(["git", "pull"])
         subprocess.call(["git", "add", "."])
         subprocess.call(["git", "commit", "-m", changedString])
